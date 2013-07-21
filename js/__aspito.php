23d6465c9ed5457647c9fc5aaf3bde3b803f3417
Carlos Cabo <carloscabo@gmail.com>
2013-07-21 12:50:30 +0200

    Cambios en __aspito.php

--- js/__aspito.php
+++ js/__aspito.php
@@ -1,11 +1,6 @@
 <?php
 
-  require $_SERVER['DOCUMENT_ROOT'] . '/_classes/debuglib.php';
-
-      //DbugL::help();
-
   require $_SERVER['DOCUMENT_ROOT'] . '/_classes/snt.aspito.php';
-
   $aspito = new Aspito();
 
 ?>
